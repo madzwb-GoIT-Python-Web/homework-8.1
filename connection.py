1b747f7bb54a5566746d15e9f2e619f5be36cdbc
Volodymyr Zavatsky <volodymyr.zawatsky@gmail.com>
2023-10-02 13:20:25 +0300

    Add python-dotenv. Switch to use dotenv with secrets folder instead of configparser.

--- connection.py
+++ connection.py
@@ -1,18 +1,31 @@
+import os
+# import configparser
+
+from dotenv import load_dotenv
 from mongoengine import connect
-import configparser
 
 
+load_dotenv()
+secret_file = os.environ.get("MONGODB_PASSWORD_FILE")
+if secret_file:
+    with open(secret_file, 'r') as fd:
+        password = ''.join([line.strip() for line in fd.readlines()])
+else:
+    password  = os.environ.get("MONGODB_PASSWORD")
 
-config = configparser.ConfigParser()
-config.read('data/config.ini')
+# config = configparser.ConfigParser()
+# config.read('data/config.ini')
 
-mongo_user = config.get('DB', 'user')
-mongodb_pass = config.get('DB', 'pass')
-db_name = config.get('DB', 'db_name')
-domain = config.get('DB', 'domain')
+# mongo_user = config.get('DB', 'user')
+# mongodb_pass = config.get('DB', 'pass')
+# db_name = config.get('DB', 'db_name')
+# domain = config.get('DB', 'domain')
+user    = os.environ.get("MONGODB_USER")
+name    = os.environ.get("MONGODB_NAME")
+domain  = os.environ.get("MONGODB_DOMAIN")
 
 connect(
-    host=f"""mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority""",
+    host=f"""mongodb+srv://{user}:{password}@{domain}/{name}?retryWrites=true&w=majority""",
     # alias="homework-8.1",
     ssl=True
 )
